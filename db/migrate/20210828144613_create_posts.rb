0e300ab75c77ec3ffda726e38fe318427831ee4c
scottms724 <scottms724@gmail.com>
2021-08-28 11:09:05 -0400

    add post model and controller logic

--- db/migrate/20210828144613_create_posts.rb
+++ db/migrate/20210828144613_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :content
-      t.string :image
+      t.string :title, null: false
+      t.text :content, null: false
+      t.string :image, default: 'https://thumbs.dreamstime.com/b/pencil-paper-4184006.jpg'
 
       t.timestamps
     end
