b9c31c512489fb321761563decc228e0b09df852
scottms724 <scottms724@gmail.com>
2021-08-28 10:46:37 -0400

    Post model generated

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,7 +5,7 @@ export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">All Blog Posts</h1>
+        <h1 className="display-4">Scott's Tech and Other Things Blog</h1>
         <p className="lead">
           Hi, I'm Scott and this is my blog on programming, tech, and how they're changing the world.
         </p>
