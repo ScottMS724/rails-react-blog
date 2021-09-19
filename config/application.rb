7a7dcda00abc043dfd7e6e42b0e488521a25196b
scottms724 <scottms724@gmail.com>
2021-09-19 11:46:47 -0400

    add new bundler require in app rb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module RailsReactRecipe
   class Application < Rails::Application
